b97d275f9f7fc889e27636edd1fa060c1937fc0e
roninadler <62499315+roninadler@users.noreply.github.com>
2020-12-29 16:47:50 +0200

    add js for user profile

---
+++ startbootstrap-sb-admin-2-gh-pages/js/userProfile.js
@@ -0,0 +1,10 @@
+var oldPasswordField= document.getElementById("inputOldPassword");
+var oldPasswordValue= oldPasswordField.value;
+
+var newPasswordField= document.getElementById("inputNewPassword");
+var newPasswordValue= newPasswordField.value;
+
+var confirmPasswordField= document.getElementById("inputConfirmPassword");
+var confirmPasswordValue= ConfirmPasswordField.value;
+
+var changePasswordNotes = document.getElementById("changePasswordNodes");
