e4baef87ef9bee212a4de154115152338c8d60df
roninadler <62499315+roninadler@users.noreply.github.com>
2020-12-30 11:22:42 +0200

    user profile
    add css
    changes in js and html in the part of the "change password"

--- startbootstrap-sb-admin-2-gh-pages/js/userProfile.js
+++ startbootstrap-sb-admin-2-gh-pages/js/userProfile.js
@@ -1,10 +1,30 @@
 var oldPasswordField= document.getElementById("inputOldPassword");
-var oldPasswordValue= oldPasswordField.value;
-
 var newPasswordField= document.getElementById("inputNewPassword");
-var newPasswordValue= newPasswordField.value;
-
 var confirmPasswordField= document.getElementById("inputConfirmPassword");
-var confirmPasswordValue= ConfirmPasswordField.value;
+// When the user clicks on any of the password fields, show the message box
+oldPasswordField.onfocus = function() {
+    document.getElementById("message").style.display = "block";
+}
 
-var changePasswordNotes = document.getElementById("changePasswordNodes");
+// When the user clicks outside of the password field, hide the message box
+oldPasswordField.onblur = function() {
+    document.getElementById("message").style.display = "none";
+}
+
+newPasswordField.onfocus = function() {
+    document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+newPasswordField.onblur = function() {
+    document.getElementById("message").style.display = "none";
+}
+
+confirmPasswordField.onfocus = function() {
+    document.getElementById("message").style.display = "block";
+}
+
+// When the user clicks outside of the password field, hide the message box
+confirmPasswordField.onblur = function() {
+    document.getElementById("message").style.display = "none";
+}
